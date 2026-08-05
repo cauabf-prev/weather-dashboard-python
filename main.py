99b6d4dc550bb6b1ebb8f2c6fd05370cd11b433d
cauabf-dev <cauabatifreire3@gmail.com>
2026-08-05 00:43:28 -0300

    feat: adiciona consulta contínua ao Weather Dashboard

--- main.py
+++ main.py
@@ -2,23 +2,37 @@
 from api.weather import buscar_clima
 
 
-cidade = input("Qual cidade deseja consultar?")                   #Usuario deve inserir a cidade na qual deseja consultar o clima 
+continuar = True
 
-resposta = buscar_clima(cidade)                          #Então chamamos a função buscar clima para que a API possa encontrar o clima da cidade
+while continuar:
+    
+ cidade = input("Qual cidade deseja consultar?")                   #Usuario deve inserir a cidade na qual deseja consultar o clima 
 
-dados = resposta.json()                                         #E em seguida nos devolve os dados em JSON e guarda na variavel DADOS
+ resposta = buscar_clima(cidade)                          #Então chamamos a função buscar clima para que a API possa encontrar o clima da cidade
 
-temperatura_celcius = (dados["main"]["temp"])       #Aqui abrimos os dados e localizamos o que queremos MAIN->TEMP, nesse caso a temperatura
+ dados = resposta.json()                                         #E em seguida nos devolve os dados em JSON e guarda na variavel DADOS
 
-local = (dados["name"])                                                                        #LOCAL
+ temperatura_celcius = (dados["main"]["temp"])       #Aqui abrimos os dados e localizamos o que queremos MAIN->TEMP, nesse caso a temperatura
 
-umidade = (dados["main"]["humidity"])                                                      #UMIDADE 
+ local = (dados["name"])                                                                       
 
-descrição = (dados["weather"][0]['description'])
+ umidade = (dados["main"]["humidity"])                                                      
+
+ descrição = (dados["weather"][0]['description'])
 
 #AGORA DEVEMOS PRINTAR TUDO PARA DARMOS A RESPOSTA AO USUÁRIO
 
-print(f"temperatura:{temperatura_celcius}")
-print(f"Local:{local}")
-print(f"umidade:{umidade}")
-print(descrição)
+ print(f"temperatura:{temperatura_celcius}")
+ print(f"Local:{local}")
+ print(f"umidade:{umidade}")
+ print(descrição)
+
+
+ confirmação = input("Deseja consultar novamente? S/N").strip().upper()
+ if confirmação == "N":
+    break
+
+ elif confirmação != "S":
+    invalides = input("Opção Inválida. Digite S/N").strip().upper()
+ 
+   
